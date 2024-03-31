91244bae8256ea7cb6cd1c19888525bab017fa3a
david <d.aparicio8383@gmail.com>
2024-03-31 11:16:40 +0200

    added ft_atoi

---
+++ libft/ft_atoi.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: davapari <davapari@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/31 11:12:07 by davapari          #+#    #+#             */
+/*   Updated: 2024/03/31 11:16:10 by davapari         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_atoi(const char *nptr)
+{
+	int	result;
+	int	signo;
+
+	result = 0;
+	signo = 1;
+	while (*nptr == ' ' || (*nptr >= 9 && *nptr <= 13))
+		nptr++;
+	if (*nptr == '-' || *nptr == '+')
+	{
+		if (*nptr == '-')
+		{
+			signo = -1 * signo;
+			nptr++;
+		}
+	}
+	while (*nptr >= '0' && *nptr <= '9')
+	{
+		result = result * 10 + (*nptr - 48);
+		nptr++;
+	}
+	return (signo * result);
+}
