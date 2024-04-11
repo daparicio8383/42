353bbfed097737a9d42c1116eaed23b5d20bb368
david <d.aparicio8383@gmail.com>
2024-04-11 06:37:58 +0200

    added ft_strdup

---
+++ libft/ft_strdup.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: davapari <davapari@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/11 06:04:19 by davapari          #+#    #+#             */
+/*   Updated: 2024/04/11 06:14:45 by davapari         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strdup(const char *s)
+{
+	char	*dup;
+	size_t	len;
+
+	len = ft_strlen(s) + 1;
+	dup = malloc(sizeof(char) * len);
+	if (!dup)
+		return (NULL);
+	else
+		return (ft_memcpy(dup, s, len));
+}
