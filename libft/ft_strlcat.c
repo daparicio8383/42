4d369653dd8726204a68a6852fd297e918844a99
david <d.aparicio8383@gmail.com>
2024-04-24 05:57:18 +0200

    updated ft_strlcat

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: davapari <davapari@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/15 09:33:25 by davapari          #+#    #+#             */
-/*   Updated: 2024/04/22 06:44:45 by davapari         ###   ########.fr       */
+/*   Updated: 2024/04/24 05:56:38 by davapari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,24 +17,24 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 	size_t	i;
 	size_t	j;
 	size_t	dst_len;
-	size_t	tot_size;
+	size_t	src_len;
 
-	i = 0;
 	j = 0;
 	dst_len = ft_strlen(dst);
-	tot_size = dst_len + ft_strlen(src);
-	while (dst[i] != '\0' && i < size)
+	src_len = ft_strlen(src);
+	i = dst_len;
+	if (size == 0)
+		return (src_len);
+	if (size < dst_len)
+		return (src_len + size);
+	else
 	{
-		i++;
+		while (src[j] && (dst_len + j) < size)
+			dst[i++] = src[j++];
+		if ((dst_len + j) == size && dst_len < size)
+			dst[--i] = '\0';
+		else
+			dst[i] = '\0';
+		return (src_len + dst_len);
 	}
-	while (src[j] != '\0' && i + j + 1 < size)
-	{
-		dst[i + j] = src[j];
-		j++;
-	}
-	if (i < size)
-	{
-		dst[i + j] = '\0';
-	}
-	return (tot_size);
 }
