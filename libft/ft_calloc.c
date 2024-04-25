9e2204cdb96d6c662a1b1c9c67630fe0f56db4b7
david <d.aparicio8383@gmail.com>
2024-04-25 06:28:59 +0200

    updated ft_calloc

--- libft/ft_calloc.c
+++ libft/ft_calloc.c
@@ -6,19 +6,19 @@
 /*   By: davapari <davapari@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/01 07:54:55 by davapari          #+#    #+#             */
-/*   Updated: 2024/04/11 06:49:49 by davapari         ###   ########.fr       */
+/*   Updated: 2024/04/25 06:28:35 by davapari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_calloc(size_t nmemb, size_t size)
+void	*ft_calloc(size_t num, size_t size)
 {
 	void	*ptr;
 
-	ptr = malloc(nmemb * size);
+	ptr = malloc(num * size);
 	if (!ptr)
 		return (NULL);
-	ft_bzero(ptr, nmemb);
+	ft_bzero(ptr, num);
 	return (ptr);
 }
